038e0d14746b8deb9927c338719cdaeffb80b3b3
GUILHERME RUFINO CAMPOS <22282090@CFP127ALUNO.LOCAL>
2023-05-19 11:13:40 -0300

    a

---
+++ js/index.js
@@ -0,0 +1,32 @@
+// crtl + d para mudar  todas variaveis ao mesmo tempo
+
+// Selecionar todos os elementos com a classe .title_about_us
+var elements = document.querySelectorAll('.title_about_us');
+
+// Iterar sobre os elementos
+for (var i = 0; i < elements.length; i++) {
+  var element = elements[i];
+
+  // Obter o texto do elemento
+  var text = element.textContent;
+
+  // Dividir o texto em palavras
+  var words = text.split(' ');
+
+  // Obter a última palavra
+  var lastWord = words[words.length - 1];
+
+  // Criar um novo elemento <span> para envolver a última palavra com a cor hexadecimal
+  var span = document.createElement('span');
+  span.style.color = '#D22020'; // Substitua '#FF0000' pelo seu código de cor hexadecimal
+
+  // Definir o texto da última palavra dentro do novo elemento <span>
+  span.textContent = lastWord;
+
+  // Substituir a última palavra no texto original pelo novo elemento <span>
+  words[words.length - 1] = span.outerHTML;
+
+  // Atualizar o conteúdo do elemento com as palavras modificadas
+  element.innerHTML = words.join(' ');
+}
+
